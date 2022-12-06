77fe0d4c18a11aa45d5f5cbfa8664d0bfd8f8cbe
mmonk3 <lawluker@gmail.com>
2022-12-06 16:24:02 +0500

    test1

--- pages/base_page.js
+++ pages/base_page.js
@@ -8,23 +8,15 @@ exports.PlaywrightDevPage = class PlaywrightDevPage {
      */
     constructor(page) {
         this.page = page;
-        this.getStartedLink = page.locator('a', { hasText: 'Get started' });
-        this.gettingStartedHeader = page.locator('h1', { hasText: 'Installation' });
-        this.pomLink = page.locator('li', { hasText: 'Guides' }).locator('a', { hasText: 'Page Object Model' });
-        this.tocList = page.locator('article div.markdown ul > li > a');
+        this.getTab = page.locator('span.eos-badge > span', {hasText: 'Вкладка 7'});
+        this.getText = page.locator('div.eos-tabs-tabpane:nth-child(7)');
     }
 
     async goto(url) {
         await this.page.goto(url);
     }
 
-    async getStarted() {
-        await this.getStartedLink.first().click();
-        await expect(this.gettingStartedHeader).toBeVisible();
-    }
-
-    async pageObjectModel() {
-        await this.getStarted();
-        await this.pomLink.click();
+    async tab_click() {
+        await this.getTab.click();
     }
 }
--- tests/test.js
+++ tests/test.js
@@ -5,19 +5,6 @@ const { PlaywrightDevPage } = require('../pages/base_page');
 test('Test 1. Tabs', async ({ page }) => {
     const Tabs_page = new PlaywrightDevPage(page);
     await Tabs_page.goto('http://89.189.152.235:1337/?path=/story/eos-tabs--default');
-    await expect(playwrightDev.tocList).toHaveText([]);
-});
-
-test('Test 2. Timepicker', async ({ page }) => {
-    const playwrightDev = new PlaywrightDevPage(page);
-    await playwrightDev.goto();
-    await playwrightDev.pageObjectModel();
-    await expect(page.locator('article')).toContainText('Page Object Model is a common pattern');
-});
-
-test('Test 3. Timepicker', async ({ page }) => {
-    const playwrightDev = new PlaywrightDevPage(page);
-    await playwrightDev.goto();
-    await playwrightDev.pageObjectModel();
-    await expect(page.locator('article')).toContainText('Page Object Model is a common pattern');
+    await page.frameLocator('#storybook-preview-iframe').getByText('Вкладка 7').click();
+    await expect(page.frameLocator('#storybook-preview-iframe').getByText('Текст 7')).toContainText('Текст 7');
 });
